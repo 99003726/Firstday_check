237b3bb0707d6311dc93e9fa28a95232cdd1f192
99003720 <78869160+99003720@users.noreply.github.com>
2021-02-11 06:11:53 +0530

    Hello World

--- sample.c
+++ sample.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello World! This is abhishek ");
+   printf("Hello World! This is abhishek\n ");
+   printf("Hi! My name is Jasleen Kaur");
    return 0;
 }
